bf54459e4c59a87d5d71d7b406256de5c865315e
ginomaglaque <ginomaglaque@gmail.com>
2021-01-15 21:00:06 -0800

    resolve conflicts

--- server/server.js
+++ server/server.js
@@ -10,13 +10,16 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
+const { authMiddleware } = require('./utils/auth');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate Apollo server with the Express application as middleware
