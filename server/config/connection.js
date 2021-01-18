d9d25ed8e3b52b8bb5f2f9172cea3579d2e6a8d5
ginomaglaque <ginomaglaque@gmail.com>
2021-01-17 18:40:14 -0800

    mongo url

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://UCLABootCamp:UCLABootCamp@cluster0.tvswp.mongodb.net/book-search?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
